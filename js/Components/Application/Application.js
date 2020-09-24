0f460124408d916766437de3dcd4df3e823235bb
dkrzoska <d.krzoska@gmail.com>
2020-09-24 23:23:00 +0200

    all calendar with change year

--- js/Components/Application/Application.js
+++ js/Components/Application/Application.js
@@ -1,65 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from "react-dom";
 import Month from "../Month/Month";
-
-const year = 2020;
-let february;
-year % 4 === 0 ? february = 29 : february = 28;
-const months = [
-    {
-        "name": "January",
-        "numberofdays": 31
-    },
-    {
-        "name": "February",
-        "numberofdays": february
-    },
-    {
-        "name": "March",
-        "numberofdays": 31
-    },
-    {
-        "name": "April",
-        "numberofdays": 30
-    },
-    {
-        "name": "May",
-        "numberofdays": 31
-    },
-    {
-        "name": "June",
-        "numberofdays": 30
-    },
-    {
-        "name": "July",
-        "numberofdays": 31
-    },
-    {
-        "name": "August",
-        "numberofdays": 31
-    },
-    {
-        "name": "September",
-        "numberofdays": 30
-    },
-    {
-        "name": "October",
-        "numberofdays": 31
-    },
-    {
-        "name": "November",
-        "numberofdays": 30
-    },
-    {
-        "name": "December",
-        "numberofdays": 31
-    }]
+import moment from "moment";
 
 function Application() {
+    const [year, setYear] = useState("2020");
+
+    const handleChangeYear = (ev) => {
+        setYear(ev.target.value);
+    }
+
+    let firstDay = moment().set("year", year);
+
+    firstDay = firstDay.startOf('year'); //ustawienie początku daty
+    // firstDay = firstDay.add(1, 'M'); //dodanie dwóch miesięcy, miesiąc != konkretniej liczbie dni, tylko zmiana miesiąca
+    // console.log(firstDay.get('d')); //pozycja pierwszego dnia wg dnia tygodnia niedziela = 0
+    // console.log(firstDay.daysInMonth());
+
+    let calendar = [];
+
+    for (let i = 0; i <= 11; i++) {
+
+        calendar.push({ "name": firstDay.format("MMMM"), "numberOfDays": firstDay.daysInMonth(), "days": [] });
+
+        let blanks = [];
+        for (let i = 0; i < firstDay.get('d'); i++) {
+            blanks.push(
+                <td>{""}</td>
+            );
+        }
+
+        let daysInMonth = [];
+        for (let day = 1; day < firstDay.daysInMonth() + 1; day++) {
+            blanks.push(
+                <td>{day}</td>
+            );
+        }
+
+        let fullArr = [...blanks, ...daysInMonth];
+        let row = [];
+        let rows = [];
+
+        fullArr.forEach((el, i) => {
+            if (i % 7 !== 0 || i === 0) {
+                row.push(el)
+            }
+            else {
+                rows.push(row);
+                row = [];
+                row.push(el);
+            }
+            if (i === fullArr.length - 1) {
+                rows.push(row)
+            }
+        });
+
+        calendar[i].days = rows.map(el => <tr>{el}</tr>)
+        firstDay.add(1, "M");
+    }
+
     return (
         <div>
-            {/* {months.map(el => <Month name = {el.name} days = {el.numberofdays}/>)} */}
-            <Month name = {months[0].name} days = {months[0].numberofdays}/>
+            <select onChange = {handleChangeYear} id="year" name="year">
+                <option value="2020">2020</option>
+                <option value="2019">2019</option>
+                <option value="2018">2018</option>
+                <option value="2017">2017</option>
+            </select>
+            {calendar.map((el, i) => <Month name={el.name} days={el.days} />)}
         </div>
     )
 }
